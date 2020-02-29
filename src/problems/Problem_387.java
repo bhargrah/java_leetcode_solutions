be3caa3dc6f451d5521b71f3f8a2f4fdc4c7ac32
Rahul Bhargava <rahulbhargava21@gmail.com>
2020-02-29 23:01:11 +0530

    387. First Unique Character in a String

---
+++ src/problems/Problem_387.java
@@ -0,0 +1,48 @@
+package problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author rahulbhargava
+ * @category 387. First Unique Character in a String
+ * @see https://leetcode.com/problems/first-unique-character-in-a-string/
+ */
+public class Problem_387 {
+
+	public static void main(String[] args) {
+
+		System.out.println(firstUniqChar("leetcode"));
+		System.out.println(firstUniqChar("loveleetcode"));
+
+	}
+
+	public static int firstUniqChar(String s) {
+
+		char[] sampleInput = s.toCharArray();
+		Map<String, Integer> wallet = new HashMap<>();
+
+		for (char sample : sampleInput) {
+
+			String key = String.valueOf(sample);
+
+			if (wallet.get(key) == null)
+				wallet.put(key, 1);
+			else
+				wallet.put(key, wallet.get(key) + 1);
+
+		}
+
+		for (int iCnt = 0; iCnt < sampleInput.length; iCnt++) {
+
+			String key = String.valueOf(sampleInput[iCnt]);
+			if (wallet.get(key) == 1)
+				return iCnt; // return first index which is non repeating
+
+		}
+
+		return -1;
+
+	}
+
+}
