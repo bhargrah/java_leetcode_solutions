dfcdb8b40f807e83ea1a86a02cd3697344f36073
Rahul Bhargava <rahulbhargava21@gmail.com>
2020-02-29 21:00:58 +0530

    Update

--- src/problems/Problem_739.java
+++ src/problems/Problem_739.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 /**
  * @author rahulbhargava
- * @category 739. Daily Temperatures 
+ * @category 739. Daily Temperatures
  * @see https://leetcode.com/problems/daily-temperatures/
  */
 public class Problem_739 {
@@ -12,23 +12,23 @@ public class Problem_739 {
 	public static void main(String[] args) {
 
 		int[] T = { 73, 74, 75, 71, 69, 72, 76, 73 };
-		
+
 		int length = T.length;
-        ArrayList<Integer> final_list = new ArrayList<>(length);
-        int [] output = new int[length];
+		ArrayList<Integer> final_list = new ArrayList<>(length);
+		int[] output = new int[length];
 
 		for (int iCnt = 0; iCnt < length; iCnt++) {
-			if(!innerLoop(T, final_list, length, iCnt))
-			  final_list.add(0); // to handle edge scenarios where there are no high temperature 
+			if (!innerLoop(T, final_list, length, iCnt))
+				final_list.add(0); // to handle edge scenarios where there are no high temperature
 		}
 
-		length = 0;
-		for(int number : final_list) output[length++] = number;	  
+		length = 0; // reusing the variable
+		for (int number : final_list) output[length++] = number;
 		System.out.println(final_list);
 	}
 
 	private static boolean innerLoop(int[] input, ArrayList<Integer> final_list, int length, int iCnt) {
-		boolean appendZero=false;
+		boolean appendZero = false;
 		for (int jCnt = iCnt + 1; jCnt < length; jCnt++) {
 			if (input[iCnt] < input[jCnt]) {
 				final_list.add(jCnt - iCnt);
