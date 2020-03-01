4a5bc4ba0e70d1a549214d9de83774d97838c04d
Rahul Bhargava <rahulbhargava21@gmail.com>
2020-03-01 15:34:17 +0530

    344. Reverse String

---
+++ src/problems/Problem_344.java
@@ -0,0 +1,28 @@
+package problems;
+
+/**
+ * @author rahulbhargava
+ * @category 344. Reverse String
+ * @see https://leetcode.com/problems/reverse-string/
+ */
+public class Problem_344 {
+
+	public static void main(String[] args) {
+		String input = "hello";
+		reverseString(input.toCharArray());
+	}
+
+	public static void reverseString(char[] s) {
+
+		int length = s.length;
+		char swap;
+
+		for (int iCnt = 0; iCnt < length / 2; iCnt++) {
+			swap = s[iCnt];
+			s[iCnt] = s[(length - 1) - iCnt];
+			s[(length - 1) - iCnt] = swap;
+		}
+
+	}
+
+}
