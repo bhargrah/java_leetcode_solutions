8998ffa9f51543960a296870a61d916ea5c3f550
Rahul Bhargava <rahulbhargava21@gmail.com>
2020-03-03 00:01:24 +0530

    268. Missing Number

---
+++ src/problems/Problem_268.java
@@ -0,0 +1,27 @@
+package problems;
+
+/**
+ * @author rahulbhargava
+ * @category 268. Missing Number
+ * @see https://leetcode.com/problems/missing-number/
+ */
+public class Problem_268 {
+
+	public static void main(String[] args) {
+		int[] input = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+		System.out.println(missingNumber(input));
+	}
+
+	public static int missingNumber(int[] nums) {
+
+		int length = nums.length;
+		int total = (length * (length + 1) / 2); // Calculating total  sum (n*(n+1))/2
+
+		for (int iCnt = 0; iCnt < length; iCnt++)
+			total = total - nums[iCnt];
+
+		return total == 0 ? 0 : total;
+
+	}
+
+}
