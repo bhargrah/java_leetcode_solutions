dd7e4a714386de5c41c52b56eebeb9c790409b01
Rahul Bhargava <rahulbhargava21@gmail.com>
2020-03-03 00:16:39 +0530

    136. Single Number

---
+++ src/problems/Problem_136.java
@@ -0,0 +1,35 @@
+package problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author rahulbhargava
+ * @category 136. Single Number
+ * @see https://leetcode.com/problems/single-number/
+ */
+public class Problem_136 {
+
+	public static void main(String[] args) {
+
+		int [] input = {4,1,2,1,2};
+		System.out.println(singleNumber(input));
+	}
+
+	public static int singleNumber(int[] nums) {
+		
+		int length = nums.length;
+		Map<Integer,Integer> dict = new HashMap<>();
+		
+		for ( int iCnt = 0 ; iCnt < length ; iCnt ++) 
+			if(dict.get(nums[iCnt]) == null) 
+				dict.put(nums[iCnt], 1); // add when not present
+			else 
+				dict.remove(nums[iCnt]); // remove if present
+		
+		// assuming that key set will have only value
+		return dict.keySet().iterator().next().intValue();
+
+	}
+
+}
