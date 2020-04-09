a7a4dbf6622e1f62a79c50b8217cd11796b3f72e
Rahul Bhargava <rahulbhargava21@gmail.com>
2020-04-09 09:30:49 +0530

    69. Sqrt(x)

---
+++ src/problems/Solution_69.java
@@ -0,0 +1,24 @@
+package problems;
+
+/**
+ * @author rahulbhargava
+ * @category 69. Sqrt(x)
+ * @see https://leetcode.com/problems/sqrtx/
+ */
+
+public class Solution_69 {
+
+	public static void main(String[] args) {
+
+		mySqrt(8);
+
+	}
+
+	public static int mySqrt(int x) {
+
+		Double result = Math.sqrt(x);
+		return result.intValue();
+
+	}
+
+}
