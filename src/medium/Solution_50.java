8448949e104b88997ed149392bcb12bd90600fe7
Rahul Bhargava <rahulbhargava21@gmail.com>
2020-08-16 16:16:58 +0530

    50. Pow(x, n)

---
+++ src/medium/Solution_50.java
@@ -0,0 +1,20 @@
+package medium;
+
+/**
+ * @author rahulbhargava
+ * @category 50. Pow(x, n)
+ * @see https://leetcode.com/problems/powx-n/
+ */
+
+public class Solution_50 {
+
+	public static void main(String[] args) {
+		myPow(2.00000, 10);
+	}
+
+	public static double myPow(double x, int n) {
+		return Math.pow(x, n);
+
+	}
+
+}
